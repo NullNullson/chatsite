77b24f1e5f2333227ef12268f72adc2f2c35a69d
NullNullson <tlaakso@eastlink.ca>
2015-10-26 13:02:58 -0400

    Updated setup file.
    Updated setup file.

--- setup.php
+++ setup.php
@@ -6,8 +6,8 @@ if($conn->connect_error){
 	die('Error: something\'s wrong with your server\'s PHP config.');
 }
 
-$conn->query('CREATE DATABASE `chattesting`');
-$conn->query('USE `chattesting`');
+$conn->query('CREATE DATABASE `chat`');
+$conn->query('USE `chat`');
 $conn->query('CREATE TABLE messages (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, message VARCHAR(100) NOT NULL)');
 
 echo 'If all went well, remove this from your server and the chat site should run. Something probably went wrong though. Good luck.';
